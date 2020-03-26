c1e8bf38f2403ae951449e291aabe1dd87f94c90
asalltech <contato.asalltech@gmail.com>
2020-03-26 19:25:30 -0300

    11 commit add

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const sqlite = require('sqlite')
 const dbconnection =sqlite.open(path.resolve(__dirname, 'Jobify.sqlite' ),{Promise})
 
 const port =process.env.PORT || 3000
+
 app.set("views",path.join(__dirname,'views'))
 app.set('view engine','ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyparser.urlencoded({extended:true}))
 
 const init = async ()=>{
